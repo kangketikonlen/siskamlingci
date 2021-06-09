664bea8701dee2ce8974cfe63fb793e6252bc5be
Kangketik <info@kangketik.web.id>
2021-06-09 20:19:47 +0700

    :zap: Social media login #2

--- application/models/Portal_model.php
+++ application/models/Portal_model.php
@@ -2,6 +2,7 @@
 class Portal_model extends CI_Model
 {
 	protected $user = "ak_data_system_user";
+	protected $user_member = "ak_data_system_user_member";
 	protected $level = "ak_data_system_level";
 	protected $info = "ak_data_system_info";
 	protected $instansi = "ak_data_system_instansi";
@@ -18,31 +19,29 @@ class Portal_model extends CI_Model
 
 	public function cek_validasi()
 	{
-		$password = $this->db->where(
-			'user_login',
-			$this->input->post('user_login')
-		)->order_by('user_id', 'desc')->get($this->user)->row('user_pass');
+		$this->db->where('user_login', $this->input->post('user_login'));
+		$this->db->order_by('user_id', 'desc');
+		$password = $this->db->get($this->user)->row('user_pass');
+
 		return password_verify($this->input->post('user_pass'), $password);
 	}
 
 	public function search_users($data)
 	{
-		$this->db->join($this->level, $this->level . '.level_id=' . $this->user . '.level_id');
-		if ($data['user_platform'] == "Github") {
-			$this->db->where($this->user . '.user_github', $data['user_platform_id']);
-		} elseif ($data['user_platform'] == "Google") {
-			$this->db->where($this->user . '.user_google', $data['user_platform_id']);
-		} elseif ($data['user_platform'] == "Twitter") {
-			$this->db->where($this->user . '.user_twitter', $data['user_platform_id']);
-		} else {
-			$this->db->where($this->user . '.user_id', $data['user_platform_id']);
-		}
-		return $this->db->order_by('user_id', 'desc')->get($this->user)->row_array();
+		$this->db->join($this->level, $this->level . '.level_id=' . $this->user_member . '.level_id');
+		$this->db->where($this->user_member . '.user_member_platform_id', $data['user_platform_id']);
+		$this->db->where($this->user_member . '.user_member_platform', $data['user_platform']);
+		return $this->db->get($this->user_member)->row_array();
 	}
 
 	public function simpan($data)
 	{
 		return $this->db->insert($this->user, $data);
+	}
+
+	public function simpan_member($data)
+	{
+		return $this->db->insert($this->user_member, $data);
 	}
 
 	public function get_user($user_login)
@@ -53,5 +52,10 @@ class Portal_model extends CI_Model
 	public function update_login($user_login)
 	{
 		return $this->db->where('user_login', $user_login)->update($this->user, array('last_login' => date('Y-m-d H:i:s')));
+	}
+
+	public function update_login_member($user_login)
+	{
+		return $this->db->where('user_member_login', $user_login)->update($this->user_member, array('last_login' => date('Y-m-d H:i:s')));
 	}
 }
