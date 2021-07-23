33c7687312dfcd544851bf8ed115b9dff91e4467
Kangketik <info@kangketik.web.id>
2021-07-23 16:50:36 +0700

    :zap: Added modul tools

---
+++ application/models/Tools/Account_model.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+class Account_model extends CI_Model
+{
+}
---
+++ application/models/Tools/Updater_model.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+class Updater_model extends CI_Model
+{
+}
