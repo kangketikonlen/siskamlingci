1cb1d4e45121ae564e8add8e2ab83dc54b05130e
Kangketik <info@kangketik.web.id>
2021-07-23 15:55:23 +0700

    :zap: Revisi background

--- application/views/dashboard/v_landing.php
+++ application/views/dashboard/v_landing.php
@@ -1,17 +1,8 @@
 <div class="row">
-	<div class="col-lg-12">
-		<div class="card">
-			<div class="card-header">
-				<div class="d-flex justify-content-between">
-					<h3 class="card-title">Selamat datang, <b><?= $this->session->userdata('nama') ?></b></h3>
-					<span id="tanggal"></span>
-				</div>
-			</div>
-			<div class="card-body">
-				<div class="d-flex">
-					<p class="text-justify">Ini adalah halaman beranda <?= $this->session->userdata('AppInfo') ?>. Silahkan jelajahi dengan menggunakan tombol navigasi di samping atau tekan tombol <i class=" fa fa-bars"></i> diatas untuk mengakses fitur.</p>
-				</div>
-			</div>
+	<div class="col col-lg-12 mt-3">
+		<div class="jumbotron py-3 text-center bg-transparent">
+			<h1>Hi, <?= $this->session->userdata('nama') ?> <i class="far fa-smile-wink"></i></h1>
+			<p class="lead">Silahkan jelajahi dengan menggunakan tombol navigasi di samping atau tekan tombol <i class=" fa fa-bars"></i> diatas untuk mengakses fitur.</p>
 		</div>
 	</div>
 </div>
